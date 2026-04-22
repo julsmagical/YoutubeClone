675291fb6911c2c033b33b519d204cd765812aef
julsmagical <julietcaro40@gmail.com>
2026-04-22 08:45:34 -0500

    fix: YoutubeCloneContext y conexión con base de datos

--- backend/with-database/YoutubeClone/YoutubeClone.Domain/Database/SqlServer/Context/YoutubeCloneContext.cs
+++ backend/with-database/YoutubeClone/YoutubeClone.Domain/Database/SqlServer/Context/YoutubeCloneContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using YoutubeClone.Domain.Database.SqlServer.Entities;
 
 namespace YoutubeClone.Domain.Database.SqlServer.Context;
@@ -51,10 +49,6 @@ public partial class YoutubeCloneContext : DbContext
     public virtual DbSet<VideoReaction> VideoReactions { get; set; }
 
     public virtual DbSet<ViewHistory> ViewHistories { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,1433;User=sa;Password=Admin1234@;Database=YoutubeClone;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
